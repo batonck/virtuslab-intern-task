a55c0d5e0cf2d791cd719985936564f19b801184
batonck <batonck007@gmail.com>
2022-03-24 23:16:48 +0100

    fixed variables

--- variables.tf
+++ variables.tf
@@ -1,23 +1,19 @@
 variable "docker_image" {
   type        = string
   description = "docker image to pull from dockerhub"
-  default = "virtuslab:1.0"
 }
 
 variable "internal_port" {
   type = number
   description = "internal port"
-  default = 80
 }
 
 variable "external_port" {
   type = number
   description = "external port"
-  default = 8081
 }
 
 variable "ip_address" {
   type = string
   description = "ip_address"
-  default = "localhost"
 }
